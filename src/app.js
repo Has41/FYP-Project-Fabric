27b9ce1fa1010cccabd94a019e93a1cab88f54b3
hassan2314 <hassan87691@gmail.com>
2024-09-20 19:51:35 +0500

    Add User Controller

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const app = express();
@@ -6,7 +7,7 @@ app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"))
-
+app.use(cookieParser())
 app.use(express.json())
 
 //import rotuers
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -77,27 +77,27 @@ userSchema.method.isPasswordCorrect = async function (password) {
 userSchema.method.generateAccessToken = async function () {
   return jwt.sign(
     {
-    _id: this._id,
-    username: this.username,
-    fullname:  this.fullname,
-    email: this.email,
-  },
-  process.env.ACCESS_TOKEN_SECRET,
-  {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-  }
+      _id: this._id,
+      username: this.username,
+      fullname: this.fullname,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
   );
 };
 
 userSchema.methods.generateRefreshToken = function () {
-    return jwt.sign(
-      {
-        _id: this._id,
-      },
-      process.env.REFRESH_TOKEN_SECRET,
-      {
-        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
-      }
-    );
-  };
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const User = mongoose.model("User", userSchema);
