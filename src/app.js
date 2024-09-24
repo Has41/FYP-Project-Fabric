8204b0df8dc6068ab3df1941899c64938e6be9a4
hassan2314 <hassan87691@gmail.com>
2024-09-24 14:32:01 +0500

    Add router

--- src/app.js
+++ src/app.js
@@ -6,15 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(express.static("public"))
-app.use(cookieParser())
-app.use(express.json())
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(express.json());
 
 //import rotuers
 
-import userRouter from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js";
+import categoryRouter from "./routes/category.route.js";
 
-app.use("/api/v1/users", userRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/categories", categoryRouter);
 
 
 export { app };
