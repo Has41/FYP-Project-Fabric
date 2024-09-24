9f0fb694bc9c256f713d57c486da8f2e851370c6
hassan2314 <hassan87691@gmail.com>
2024-09-24 15:28:36 +0500

    Update app.js

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -4,9 +4,8 @@ import { verifyJwt } from "../middleware/auth.middleware.js";
 import { addProduct } from "../controlllers/product.controller.js";  // Assuming this is where addProduct is located
 
 const router = Router();
+router.use(verifyJwt);
 
-// Route to add a product
-// Applies authentication middleware (verifyJwt) and file upload middleware (upload)
-router.post('/add', verifyJwt, upload.array('images', 4), addProduct);
+router.post('/add', upload.array('images', 4), addProduct);
 
 export default router;
