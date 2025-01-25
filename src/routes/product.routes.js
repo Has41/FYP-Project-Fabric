807dbfdafe22ba3857a14833f46a163ad2c414ec
hassan2314 <hassan87691@gmail.com>
2025-01-25 21:27:46 +0500

    Adjust Routes of Product

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,27 +1,24 @@
 import { Router } from "express";
-import { upload } from "../middleware/multer.middleware.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
-
+import { adminOnly } from "../middleware/admin.middleware.js";
 import {
   addProduct,
   allProducts,
   removeProduct,
   searchProduct,
   updateProductInfo,
-  
 } from "../controllers/product.controller.js";
 
 const router = Router();
 router.use(verifyJwt);
 
-router.post("/add", addProduct);
+router.route("/add").post(adminOnly, addProduct);
 
-router.delete("/:productId", removeProduct);
+router.route("/:productId").delete(adminOnly, removeProduct);
 
-router.put("/update/:productId", updateProductInfo);
+router.route("/update/:productId").put(adminOnly, updateProductInfo);
 
-router.get('/get/:productId', searchProduct);
-
+router.get("/get/:productId", searchProduct);
 
 router.get("/all-products", allProducts);
 
